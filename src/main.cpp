39d281244c8052f8d29c958e5bb3d6e4227fa1b9
Trung Le <trungle@seas.upenn.edu>
2019-06-24 03:28:23 -0700

    Updated TBB parallelization

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <chrono>
 #include <fstream>
+#include <iostream>
 #include <glm/glm.hpp>
 #include <tbb/tbb.h>
 
@@ -9,6 +10,9 @@
 #include "sampler/sampler.h"
 #include "screen/screen.h"
 #include "utility.h"
+
+#define PARALLEL 1
+#define REPORT 1
 
 using namespace glm;
 using namespace std;
@@ -87,7 +91,9 @@ void RenderFullscreen()
 	file.open("../images/image.ppm");	
 
 	// Screen
-	g_screen = new Screen(800, 600);
+	const int nx = 800;
+	const int ny = 600;
+	g_screen = new Screen(nx, ny);
 	file << "P3\n" << g_screen->width << " " << g_screen->height << "\n255\n";
 
 	// Camera
@@ -95,18 +101,70 @@ void RenderFullscreen()
 	vec3 lookAt = vec3(0, 0, 0);
 	float vfov = 40;
 	float aspect = float(g_screen->width) / float(g_screen->height);
-	float aperture = 0.00f;
+	float aperture = 0.02f;
 	float focusDist = length(lookAt - lookFrom);
 	g_camera = new Camera(lookFrom, lookAt, vfov, aspect, aperture, focusDist);
 	int numSamplesPerPixel = 100;
+
+	// Begin render
+#if REPORT
+	auto start = chrono::steady_clock::now();
+#endif
+
+#if PARALLEL
+
+	vec3 imageBuffer[nx][ny];
+
+	parallel_for(blocked_range2d<int>(0, g_screen->height, 0, g_screen->width),
+		[numSamplesPerPixel, &imageBuffer](blocked_range2d<int> range)
+		{
+			int rowEnd = range.rows().end();
+			for (int y = range.rows().begin(); y < rowEnd; y++)
+			{
+				int colEnd = range.cols().end();
+				for (int x = range.cols().begin(); x < colEnd; x++)
+				{
+					SumColor sumColor(x, y);
+					parallel_reduce(blocked_range<size_t>(0, numSamplesPerPixel), sumColor);
+					vec3 color = sumColor._sumColor;
+					color /= float(numSamplesPerPixel);
+
+					// Gamma correction
+					color = glm::sqrt(color);
+					imageBuffer[x][y] = color;
+
+				}
+			}
+		});
+
+	// Output to file
 
 	for (int y = g_screen->height - 1; y >= 0; y--)
 	{
 		for (int x = 0; x < g_screen->width; x++)
 		{
-			SumColor sumColor(x, y);
-			parallel_reduce(blocked_range<size_t>(0, numSamplesPerPixel), sumColor);
-			vec3 color = sumColor._sumColor;
+			vec3 color = imageBuffer[x][y];
+			int ir = int(color.r * 255.99);
+			int ig = int(color.g * 255.99);
+			int ib = int(color.b * 255.99);
+			file << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+
+#else
+
+	for (int y = g_screen->height - 1; y >= 0; y--)
+	{
+		for (int x = 0; x < g_screen->width; x++)
+		{
+			vec3 color(0.0f, 0.0f, 0.0f);
+			for (int n = 0; n < numSamplesPerPixel; n++)
+			{
+				vec2 uv = Sampler::RandomSampleFromPixel(x, y, g_screen->width, g_screen->height);
+				Ray r = g_camera->GetRay(uv);
+				color += Shade(r, 50);
+			}
+
 			color /= float(numSamplesPerPixel);
 
 			// Gamma correction
@@ -119,7 +177,24 @@ void RenderFullscreen()
 			file << ir << " " << ig << " " << ib << "\n";
 		}
 	}
+#endif
 
+
+#if REPORT
+	auto end = chrono::steady_clock::now();
+	cout << "Render " << g_screen->width << "x" << g_screen->height << " image took " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl;
+	cout << "	" << numSamplesPerPixel << " samples / pixel" << endl;
+	cout << "Scene: " << endl;
+	cout << "	Camera: " << endl;
+	cout << "		Look from: " << lookFrom.x << ", " << lookFrom.y << ", " << lookFrom.z << endl;
+	cout << "		Look at: " << lookAt.x << ", " << lookAt.y << ", " << lookAt.z << endl;
+	cout << "		Aspect: " << aspect << endl;
+	cout << "		FOV: " << vfov << endl;
+	cout << "		Aperture: " << aperture << endl;
+	cout << "		Focus distance: " << focusDist << endl;
+	cout << "	" << g_scene.objects.size() << " objects" << endl;
+	cout << "	" << g_scene.materials.size() << " materials" << endl;
+#endif
 	file.close();
 }
 
